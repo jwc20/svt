424b4b4e7fe7f5f1e9ee071458923de09e629ac3
jw <choi.jae100@gmail.com>
2026-04-09 14:22:34 -0700

    Add Player and Inventory struct

--- game.go
+++ game.go
@@ -2,3 +2,16 @@ package svt
 
 const TotalRequiredMileage = 2040
 const InitialCash = 700
+
+type Player struct {
+	Cash          int
+	ShootingLevel int
+}
+
+type Inventory struct {
+	Oxen          int
+	Food          int
+	Ammo          int
+	Clothing      int
+	Miscellaneous int
+}
